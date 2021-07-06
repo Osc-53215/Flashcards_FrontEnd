78c68423f88691c005eb5bb48b7bc201b0885305
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-07-05 20:44:57 -0500

    created label and text area in creatcard component

--- flashcard/src/CreateCard.js
+++ flashcard/src/CreateCard.js
@@ -3,10 +3,16 @@ import './CreateCard.css';
 
 function CreateCard() {
     return (
+        <>
         <div className = 'createcards'>
             <h2>Create Flashacards</h2>
-            
         </div>
+
+        <div className = 'creatcards__label'>
+            <label for = 'question'>Questions</label>
+            <textarea name = '' id = '' cols = '30' rows = '10' />
+        </div>
+        </>
     )
 }
 
