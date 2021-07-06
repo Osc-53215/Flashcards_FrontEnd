7442a575f63d9556891bc33d914a322021e41103
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-07-05 20:16:43 -0500

    created button div tags

---
+++ flashcard/src/Button.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Button() {
+    return (
+        <div className = 'button'>
+
+            <div className = 'button__new'>
+                
+            </div>
+
+            <div className = 'button__del'>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Button
