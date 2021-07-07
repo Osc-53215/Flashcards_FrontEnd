8e3f5f475becf41305ccef4986a8ce1f54915784
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-07-07 15:59:52 -0500

    added the axios get request

--- flashcard/src/App.js
+++ flashcard/src/App.js
@@ -2,9 +2,20 @@ import './App.css';
 import Header from './Header';
 import CreateCard from './CreateCard';
 import React, { Component }  from 'react';
+import axios from 'axios';
 
 
 class App extends Component{
+
+  flashcard = {
+      flashcards: []
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:8000/api/flashcard').then(res => {
+      this.setState({flashcards: res.data})
+    })
+  }
   
   render(){
     return (
